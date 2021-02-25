964e98748da36d3ab5efdf84193ba905664fa0ea
rtmigo <art.galkin@gmail.com>
2021-02-25 18:24:44 +0300

    ...

--- lib/src/02_ramPrefsStorage.dart
+++ lib/src/02_ramPrefsStorage.dart
@@ -1,6 +1,6 @@
-// Copyright (c) 2021 Artёm Galkin.
-// Use of this source code is governed by a MIT license.
-// See LICENSE file for details
+// Copyright (c) 2021 Artёm Galkin. All rights reserved.
+// Use of this source code is governed by the BSD-3-Clause license found
+// in the LICENSE file in the root directory of this source tree
 
 import '01_prefsStorage.dart';
 
