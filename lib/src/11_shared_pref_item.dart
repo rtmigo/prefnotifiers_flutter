267c47a3c15230275ad99161f8de44386da40c9e
rtmigo <art.galkin@gmail.com>
2021-03-24 21:40:06 +0300

    adding sound null safety

--- lib/src/11_shared_pref_item.dart
+++ lib/src/11_shared_pref_item.dart
@@ -4,5 +4,11 @@ class SharedPref<T> extends PrefItem<T> {
   SharedPref(String key, {
     T Function()? initFunc,
     checkValue,
-  }): super(SharedPrefsStorage(), key, initFunc: initFunc, checkValue: checkValue);
+  }): super(_commonStorage, key, initFunc: initFunc, checkValue: checkValue);
+
+  static SharedPrefsStorage? _commonStorageVal;
+  static SharedPrefsStorage get _commonStorage {
+    return _commonStorageVal ??= SharedPrefsStorage();
+    //return _commonStorageVal;
+  }
 }
