43b44e98e4551e5b3acc8d4e0fbcb061fdf756f8
rtmigo <art.galkin@gmail.com>
2021-03-25 03:51:13 +0300

    adding sound null safety

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# 1.0.1-beta
+# 1.0.2-beta
 
 - Migrated to sound null safety
 - Added PrefNotifier class as a shortcut to PrefItem(SharedPrefsStorage(), ...)
--- lib/src/11_shared_pref_item.dart
+++ lib/src/11_shared_pref_item.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:prefnotifiers/prefnotifiers.dart';
 
 class PrefNotifierTypeError extends TypeError {
