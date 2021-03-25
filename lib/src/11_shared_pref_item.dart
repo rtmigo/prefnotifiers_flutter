6505f6721192672082ae51a7714921916963d617
rtmigo <art.galkin@gmail.com>
2021-03-25 12:57:50 +0300

    1.0.3

--- lib/src/11_shared_pref_item.dart
+++ lib/src/11_shared_pref_item.dart
@@ -1,30 +1,19 @@
+// Copyright (c) 2021 Artёm Galkin <github.com/rtmigo>. All rights reserved.
+// Use of this source code is governed by the BSD-3-Clause license found
+// in the LICENSE file in the root directory of this source tree
+
 import 'package:prefnotifiers/prefnotifiers.dart';
 
-class PrefNotifierTypeError extends TypeError {
-  String key;
-  Type type;
-  Type typeB;
-  PrefNotifierTypeError(this.key, this.type, this.typeB);
-
-  @override
-  String toString() {
-    return 'A $type has already been declared for key "$key". '
-        'Cannot create PrefNotifier<$typeB> for the same key.';
-
-
-  }
-}
-
 class PrefNotifier<T> extends PrefItem<T> {
-  PrefNotifier(String key, {
+  PrefNotifier(
+    String key, {
     T Function()? initFunc,
     checkValue,
-  }
-  ): super(_commonStorage, key, initFunc: initFunc, checkValue: checkValue);
+  }) : super(_commonStorage, key, initFunc: initFunc, checkValue: checkValue);
 
   static SharedPrefsStorage? _commonStorageVal;
+
   static SharedPrefsStorage get _commonStorage {
     return _commonStorageVal ??= SharedPrefsStorage();
   }
-
 }
